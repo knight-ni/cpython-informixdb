cff202f0bfe54bd4f984079a25a3e9f6c3a241cd
Knight.Ni <knightnibo@gmail.com>
2021-12-02 15:27:18 +0800

    more

--- upload.sh
+++ upload.sh
@@ -4,4 +4,5 @@ rm -rf __pycache__
 rm -rf InformixDB.egg-info
 /usr/local/python3/bin/python3.9 setup.py sdist
 /usr/local/python3/bin/python3.9 setup.py bdist_wheel --universal
+/usr/local/python3/bin/python3.9 -m twine upload dist/InformixDB-Knight-3.tar.gz --verbose
 
