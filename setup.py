3720a10f26790a1426471d6d2f894ea538433cac
Knight.Ni <knightnibo@gmail.com>
2021-12-03 14:03:34 +0800

    more

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ if sys.version < '2.2.3':
     DistributionMetadata.download_url = None
 
 setup (name = 'InformixDB-Knight',
-       version = '3.0.0.1',
+       version = '3.0.0.2',
        description = 'InformixDB v3.0',
        long_description = \
          "InformixDB is a DB-API 2.0 compliant interface for IBM Informix Work For Python3\n"
