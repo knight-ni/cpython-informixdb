6338685b4abcaf482d6c168878a7b721b5f69504
Knight.Ni <knightnibo@gmail.com>
2021-12-12 11:23:46 +0800

    wsgi added

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ if sys.version < '2.2.3':
     DistributionMetadata.download_url = None
 
 setup (name = 'InformixDB-Knight',
-       version = '3.0.0.6',
+       version = '3.0.0.7',
        description = 'InformixDB v3.0',
        long_description = \
          "InformixDB is a DB-API 2.0 compliant interface for IBM Informix Work For Python3\n"
